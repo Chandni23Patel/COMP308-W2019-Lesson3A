9a492446ca89e07a9596186eb8d5bb8fe4b8f3b9
chandni <chandptl.91@gmail.com>
2019-01-26 11:59:57 -0500

    restructured project

---
+++ public/Scripts/app.js
@@ -0,0 +1,9 @@
+//import { start } from "repl";
+
+/* custom JavaScripts goes here */
+(function(){
+    function Start(){
+        console.log(`%c App Start`, "font-size: 20px; color: blue; font-weight: bold");
+    }
+    window.addEventListener("load",Start);
+});
